5e3fd3b8b0a2b1c242537868ea3664ee08b894b9
IC-FogClear <55770016+IC-FogClear@users.noreply.github.com>
2019-09-25 11:33:23 +1000

    Add files via upload

---
+++ Geometry.py
@@ -0,0 +1,23 @@
+﻿#qpy:console
+#-*-coding:utf8;-*-
+#
+
+
+GEOMETRY	
+
+Shape():
+	Triangle 
+	Circle 
+	Square
+	Hexagon
+
+Shapes:
+	Shape.Triangle
+	Shape.Square
+	Shape.circle
+	Shape.Hexagon
+
+Plane:
+	Plane.1D
+	Plane.2D
+	Plane.3D
