06d8e96c95a1dc24c3ff5676a1c23275fc6a932e
IC-FogClear <55770016+IC-FogClear@users.noreply.github.com>
2019-11-20 17:57:01 +1100

    Update Geometry.py

--- Geometry.py
+++ Geometry.py
@@ -19,3 +19,13 @@ Shapes:
 Plane:
 	"Plane.2D()" "x(y)"
 	"Plane.3D()" "x(y)", "z()"
+
+------------------- 
+#*__*coding:utf-8;*__*
+#qpython:console
+
+def science():
+    atom = 'aⁿ()', particle = 'pⁿ()', molecule = 'mⁿ()',
+    STRUCTURAL_INTEGRITY = ((h*l*d*w*¿)height,length,depth,width)
+    maybe ?? 
+
