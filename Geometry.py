230a9ae2d1212414295221b2ed34c7538c68c41a
IC-FogClear <55770016+IC-FogClear@users.noreply.github.com>
2020-02-13 12:55:35 +1100

    Update Geometry.py

--- Geometry.py
+++ Geometry.py
@@ -2,30 +2,10 @@
 #qpy:console
 #qpy:3
 
+# setup:main
 
-def GEOMETRY():	
-    Shape = (
-    "Triangle()"
-    "Circle()"
-    "Square()"
-    "Hexagon()"
- )
-Shapes:
-	Shape.Triangle
-	Shape.Square
-	Shape.circle
-	Shape.Hexagon
-
-Plane:
-	"Plane.2D()" "x(y)"
-	"Plane.3D()" "x(y)", "z()"
-
-------------------- 
-#*__*coding:utf-8;*__*
-#qpython:console
-
-def science():
-    atom = 'aⁿ()', particle = 'pⁿ()', molecule = 'mⁿ()',
-    STRUCTURAL_INTEGRITY = ((h*l*d*w*¿)height,length,depth,width)
-    maybe ?? 
+"""
+polygon = '[shape]'
+shape-2d = ["square","triangle","rectangle","oval","hexagon"]
+"""
 
